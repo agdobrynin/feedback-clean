a154af4c041982cd1aa0affabe9495ba27a2f62a
Alexey Dobrynin <ag.dobrynin@gmail.com>
2020-08-25 20:15:48 +0400

    Remove try catch in Controllers, now use global try catch in router

--- src/App/Controller/FeedbackList.php
+++ src/App/Controller/FeedbackList.php
@@ -13,18 +13,12 @@ final class FeedbackList extends Controller
     public function __invoke(): Response
     {
         $response = new Response();
-        try {
-            // Выдать новый Csrf токен
-            $data['csrf'] = $this->getCsrf()->refresh($response);
-            // Работа с коллекцией сообщений
-            $messageCollection = new Messages($this->getPdo());
-            $data['pages'] = ceil($messageCollection->getTotal() / Messages::PAGE_SIZE);
-            $result = $this->getView()->render('list', $data);
-        } catch (\Throwable | \PDOException $exception) {
-            $response->setStatusCode(500, 'Server Error!');
-            $result = $exception->getMessage();
-        }
+        // Выдать новый Csrf токен
+        $data['csrf'] = $this->getCsrf()->refresh($response);
+        // Работа с коллекцией сообщений
+        $messageCollection = new Messages($this->getPdo());
+        $data['pages'] = ceil($messageCollection->getTotal() / Messages::PAGE_SIZE);
 
-        return $response->setBody($result);
+        return $response->setBody($this->getView()->render('list', $data));
     }
 }
--- src/App/Controller/FeedbackSave.php
+++ src/App/Controller/FeedbackSave.php
@@ -13,20 +13,13 @@ final class FeedbackSave extends Controller
     public function __invoke(): Response
     {
         $response = new Response();
-        try {
-            // проверми csrf ключ и выставим новый если все ок
-            $this->getCsrf()->verify()->refresh($response);
-            // Создаю Entity и валидирую данные с POST
-            $message = (new Message($_POST))->validate();
-            $stm = $this->getPdo()->prepare($message->getSql());
-            $stm->execute($message->getStmData());
-            $data = ['success' => sprintf('Спасибо, %s', $message->getName())];
-            $response->setStatusCode(200);
-        } catch (\UnexpectedValueException | \PDOException $exception) {
-            $data = ['error' => $exception->getMessage()];
-            $response->setStatusCode(500, 'Server error');
-        }
+        // проверми csrf ключ и выставим новый если все ок
+        $this->getCsrf()->verify()->refresh($response);
+        // Создаю Entity и валидирую данные с POST
+        $message = (new Message($_POST))->validate();
+        $stm = $this->getPdo()->prepare($message->getSql());
+        $stm->execute($message->getStmData());
 
-        return $response->setJson($data);
+        return $response->setJson(['success' => sprintf('Спасибо, %s', $message->getName())]);
     }
 }
